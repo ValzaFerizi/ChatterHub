4540d1889620c1e572f0ae421b643eb9aabf4a34
aldamuhadri <am70315@ubt-uni.net>
2026-05-30 20:37:14 +0200

    feat: add input validation middleware

---
+++ src/middleware/validationMiddleware.js
@@ -0,0 +1,29 @@
+const { body, validationResult } = require('express-validator');
+
+const validateRegister = [
+  body('first_name').notEmpty().withMessage('First name is required'),
+  body('last_name').notEmpty().withMessage('Last name is required'),
+  body('email').isEmail().withMessage('Valid email is required'),
+  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  }
+];
+
+const validateLogin = [
+  body('email').isEmail().withMessage('Valid email is required'),
+  body('password').notEmpty().withMessage('Password is required'),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  }
+];
+
+module.exports = { validateRegister, validateLogin };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser, refreshToken, logoutUser } = require('../controllers/authController');
 const { verifyToken } = require('../middleware/authMiddleware');
+const { validateRegister, validateLogin } = require('../middleware/validationMiddleware');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
